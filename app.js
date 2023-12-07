6549ed19c8bce88d7c4c6876fbe2bd2c413ea27a
Vítor Cunha <142564603+VitorCunhaDePaula@users.noreply.github.com>
2023-12-07 15:24:40 -0300

    Update app.js
    Comentários adicionados

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
-(function (){
-const input = document.querySelectorAll(".formulario");
-const form = document.querySelector("form")
-const checkbox = document.querySelector("#check");
-const sendFormulary = document.querySelector(".sendFormulary");
-checkbox.addEventListener("click", function(){
-    sendFormulary.disabled = !checkbox.checked;
-})
+(function () {
+    // Seleciona todos os elementos com a classe "formulario"
+    const input = document.querySelectorAll(".formulario");
+    // Seleciona o formulário na página
+    const form = document.querySelector("form");
+    // Seleciona o checkbox com o ID "check"
+    const checkbox = document.querySelector("#check");
+    // Seleciona o elemento com a classe "sendFormulary"
+    const sendFormulary = document.querySelector(".sendFormulary");
 
-    form.addEventListener("submit", function(e){
-    e.preventDefault();
-    let formIsValid = true;
-    input.forEach(campo => {
-        if (campo.value.trim() === "") {
-            campo.style.border = "2px red solid";
-            formIsValid = false;
-        } else {
-            campo.style.border = "";
-        }
+    // Adiciona um ouvinte de evento para o clique no checkbox
+    checkbox.addEventListener("click", function () {
+        // Desativa o botão de envio se o checkbox não estiver marcado
+        sendFormulary.disabled = !checkbox.checked;
     });
 
-    if (formIsValid) {
-        alert("Formulário enviado");
-    }
-});
+    // Adiciona um ouvinte de evento para o envio do formulário
+    form.addEventListener("submit", function (e) {
+        // Impede o comportamento padrão de envio do formulário
+        e.preventDefault();
+        
+        // Flag para verificar se o formulário é válido
+        let formIsValid = true;
 
+        // Itera sobre cada campo de entrada no formulário
+        input.forEach(campo => {
+            // Se o campo estiver vazio, adiciona uma borda vermelha e marca o formulário como inválido
+            if (campo.value.trim() === "") {
+                campo.style.border = "2px red solid";
+                formIsValid = false;
+            } else {
+                // Se o campo estiver preenchido, remove a borda vermelha
+                campo.style.border = "";
+            }
+        });
 
-
-
-})()
+        // Se o formulário for válido, exibe um alerta indicando que foi enviado
+        if (formIsValid) {
+            alert("Formulário enviado");
+        }
+    });
+})();
